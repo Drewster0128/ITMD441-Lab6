459d9af03c37d1e3b4e8281279d2df992a985431
Drewster0128 <acook21@hawk.iit.edu>
2025-04-22 12:02:42 -0500

    Create getters for tomorrow sunrise and sunset

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ class Location
 {
     #sunRiseToday;
     #sunSetToday;
+    #sunRiseTomorrow;
+    #sunSetTomorrow;
     #name;
     #latitude;
     #longitude;
@@ -12,22 +14,23 @@ class Location
         this.#name = name;
         this.#latitude = latitude;
         this.#longitude = longitude;
-        this.#sunRiseToday = null;
-        this.#sunSetToday = null;
     }
 
     async #fetchSunData()
     {
-        let response = await fetch(`https://api.sunrisesunset.io/json?lat=${this.#latitude}&lng=${this.#longitude}`);
+        let response = await fetch(`https://api.sunrisesunset.io/json?lat=${this.#latitude}&lng=${this.#longitude}&date_start=today&date_end=tomorrow`);
         response = await response.json();
-        this.#sunRiseToday = response["results"]["sunrise"];
-        this.#sunSetToday = response["results"]["sunset"];
+
+        this.#sunRiseToday = response.results[0].sunrise;
+        this.#sunSetToday = response.results[0].sunset;
+        this.#sunRiseTomorrow = response.results[1].sunrise;
+        this.#sunSetTomorrow = response.results[1].sunset;
     }
 
     get sunRiseToday()
     {
         return (async() => {
-            if(this.#sunRiseToday == null)
+            if(!this.#sunRiseToday)
             {
                 await this.#fetchSunData();
             }
@@ -38,15 +41,43 @@ class Location
     get sunSetToday()
     {
         return (async() => {
-            if(this.#sunSetToday == null)
+            if(!this.#sunSetToday)
             {
                 await this.#fetchSunData();
             }
             return this.#sunSetToday;
         })();
     }
+
+    get sunRiseTomorrow()
+    {
+        return (async() => {
+            if(!this.#sunRiseTomorrow)
+            {
+                await this.#fetchSunData();
+            }
+            return this.#sunRiseTomorrow;
+        })();
+    }
+
+    get sunSetTomorrow()
+    {
+        return (async() => {
+            if(!this.#sunSetTomorrow)
+            {
+                await this.#fetchSunData();
+            }
+            return this.#sunSetTomorrow;
+        })();
+    }
 }
 
 let chicago = new Location("chicago", 41.85003, -87.65005);
-chicago.sunRiseToday.then((sunRise) => console.log(sunRise));
+Promise.all([chicago.sunRiseToday, chicago.sunSetToday, chicago.sunRiseTomorrow, chicago.sunSetTomorrow])
+    .then((values) => {
+        for(let i = 0; i < values.length; i++)
+        {
+            console.log(values[i]);
+        }
+    });
 
